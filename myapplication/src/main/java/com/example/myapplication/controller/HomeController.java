60d5f529360c67a37e4ea2f5ab1c5417af61b2b1
Bushan SC <bushan1992@zohomail.in>
2025-01-07 20:40:27 +0530

    Added day 25 notes

--- myapplication/src/main/java/com/example/myapplication/controller/HomeController.java
+++ myapplication/src/main/java/com/example/myapplication/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //controller class
 @Controller
@@ -16,7 +17,7 @@ public class HomeController {
     public String showHomePage(Model model) {
         model.addAttribute("myname", "Bushan");
         model.addAttribute("title", "Home");
-        return "mypage";
+        return "home";
     }
 
     @GetMapping("/about-us")
@@ -35,5 +36,15 @@ public class HomeController {
     public String showServicesPage(Model model) {
         model.addAttribute("title", "Services");
         return "services";
+    }
+
+    @GetMapping("/products")
+    public String showProductsPage(Model model, @RequestParam(required = false, defaultValue = "Unknown Product") String name,
+                                   @RequestParam(required = false, name = "price", defaultValue = "0.00") Double price) {
+        System.out.println("Receiving the parameter from URL::::"+name);
+        System.out.println("Receiving the parameter from URL::::"+price);
+        model.addAttribute("title", "Products");
+        model.addAttribute("appname", name);
+        return "products";
     }
 }
