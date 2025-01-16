bac67dc7e8c75ffd624aaef673530cb2ab8650f2
Bushan SC <bushan1992@zohomail.in>
2025-01-16 10:49:06 +0530

    Added 41, 42

---
+++ bootstrapintegration/src/main/java/com/example/bootstrapintegration/BootstrapintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstrapintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapintegrationApplication.class, args);
+	}
+
+}
---
+++ bootstrapintegration/src/test/java/com/example/bootstrapintegration/BootstrapintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootstrapintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
