cd5cfaa14f530dd45288ef343bac137b9df1f35b
Bushan SC <bushan1992@zohomail.in>
2025-01-16 20:20:57 +0530

    Added 43

---
+++ templateintegration/src/test/java/com/example/templateintegration/TemplateintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.templateintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
