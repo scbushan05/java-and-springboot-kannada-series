8dea136a2c0634d73eadb45918068565103910d0
Bushan SC <bushan1992@zohomail.in>
2025-01-21 20:47:21 +0530

    Added 45 code

--- templateintegration/src/main/java/com/example/templateintegration/controller/HomeController.java
+++ templateintegration/src/main/java/com/example/templateintegration/controller/HomeController.java
@@ -1,13 +1,33 @@
 package com.example.templateintegration.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping({"/home", "", "/"})
-    public String showHomePage() {
-        return "home";
+    public String showHomePage(Model model) {
+        model.addAttribute("title", "Home");
+        return "master";
+    }
+
+    @GetMapping("/services")
+    public String showServicesPage(Model model) {
+        model.addAttribute("title", "Services");
+        return "master";
+    }
+
+    @GetMapping("/about")
+    public String showAboutPage(Model model) {
+        model.addAttribute("title", "About");
+        return "master";
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage(Model model) {
+        model.addAttribute("title", "Contact");
+        return "master";
     }
 }
