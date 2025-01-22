10c92461f9b1a8b5878c79cf5f0a996c8e588184
Bushan SC <bushan1992@zohomail.in>
2025-01-22 20:23:55 +0530

    jdbc integration initial commit

---
+++ jdbctemplatedemo/src/test/java/com/example/jdbctemplatedemo/JdbctemplatedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbctemplatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbctemplatedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
