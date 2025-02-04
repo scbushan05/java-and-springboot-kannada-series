f0164852c7df76630fd7ff4f727e2ebc731270ac
Bushan SC <bushan1992@zohomail.in>
2025-02-04 07:07:39 +0530

    Added day 49 class code

--- jdbctemplatedemo/src/main/java/com/example/jdbctemplatedemo/controller/HomeController.java
+++ jdbctemplatedemo/src/main/java/com/example/jdbctemplatedemo/controller/HomeController.java
@@ -2,9 +2,11 @@ package com.example.jdbctemplatedemo.controller;
 
 import com.example.jdbctemplatedemo.model.User;
 import com.example.jdbctemplatedemo.service.RegisterService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,8 +24,11 @@ public class HomeController {
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute User user) {
+    public String register(@Valid @ModelAttribute User user, BindingResult result) {
         System.out.println("Printing the user details:"+user);
+        if (result.hasErrors()) {
+            return "register";
+        }
         registerService.register(user);
         return "success";
     }
--- jdbctemplatedemo/src/main/java/com/example/jdbctemplatedemo/model/User.java
+++ jdbctemplatedemo/src/main/java/com/example/jdbctemplatedemo/model/User.java
@@ -1,18 +1,22 @@
 package com.example.jdbctemplatedemo.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class User {
 
+    @NotBlank(message = "Please enter the full name")
     private String fullName;
+    @NotBlank(message = "Please enter email address")
+    @Email(message = "Please enter valid email address")
     private String email;
     private String password;
     private String gender;
     private String hobbies;
     private String dob;
     private String country;
+    @Min(value = 10, message = "Mobile number must be 10 digit")
     private String phone;
-
 
 }
